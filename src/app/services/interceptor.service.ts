0ac04d0b8bf792cb511cae0c3799f8e7868c3fef
Saeed Ganji <shahabganji@gmail.com>
2018-06-26 02:08:46 +0430

    code(intercepter)

---
+++ src/app/services/interceptor.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AccountService } from './account.service';
+import { ConfigService } from './config.service';
+import { IStock } from './stocks.model';
+
+@Injectable()
+export class StockInterceptor implements HttpInterceptor {
+
+  constructor(private accountService: AccountService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    const request = req.clone();
+
+    request.headers.append('Accept', 'application/json');
+
+    return next.handle(request).do(event => {
+
+      console.log(event);
+
+      if (event instanceof HttpResponse && event.url === ConfigService.get('api')) {
+
+        const stocks = event.body as Array<IStock>;
+
+        const symbols = this.accountService.stocks.map(stock => stock.symbol);
+
+        stocks.forEach(stock => {
+          this.accountService.stocks.map(item => {
+            if (stock.symbol === item.symbol) {
+              item.price = stock.price;
+              item.change = ((stock.price * 100) - (item.cost * 100)) / 100;
+            }
+          });
+        });
+        this.accountService.calculateValue();
+        return stocks;
+
+      }
+    });
+  }
+}
